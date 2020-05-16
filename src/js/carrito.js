3b1a7f767628c9211ab931b7a3633bc9191513a4
Isam Espinosa <isamdavid17@gmail.com>
2020-05-15 21:38:43 -0500

    Se agrego código js para agregar al carrito los productos

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -19,12 +19,62 @@ function hideShowCar() {
 
 //variables
 const car = document.querySelector('.header__car--icon')
-const listProducts = document.querySelector('.main')
+const products = document.querySelector('.main')
+const listProduct = document.querySelector('#car__list tbody')
 
 console.log(car)
-console.log(listProducts)
+console.log(products)
 
 //listeners
+chargeEventListeners()
+
+function chargeEventListeners(){
+  //Dispara cuando presionamos "Añadir"
+  products.addEventListener('click',buyProducts);
+}
 
 
 //funciones
+
+//funci´n que añade producto al carrito 
+function buyProducts(e) {
+  e.preventDefault();
+  //Delegation para agregar al carrito
+  if (e.target.classList.contains('card__button')){
+    const product = e.target.parentElement.parentElement;
+    //Enviamos el curso seleccionado para tomar sus datos 
+  readProduct(product)
+  }
+  
+}
+
+//Esta función lee el producto
+
+function readProduct(product){
+  const dataProduct = {
+    img: product.querySelector('img').src,
+    name: product.querySelector('h3').textContent,
+    price: product.querySelector('.price').textContent,
+    id: product.querySelector('input').getAttribute('id')
+  }
+
+  insertCar(dataProduct);
+  console.log(dataProduct)
+  
+}
+
+//Muestra el producto seleccionado en el carrito
+function insertCar(product) {
+  const row = document.createElement('tr')
+  row.innerHTML = `
+    <td class="navCar__img">
+      <img src="${product.img}">
+    </td>
+    <td>${product.name}</td>  
+    <td>${product.price}</td>  
+    <td>
+      <a href="#" class="borrar-curso" id="${product.id}">X </a> 
+    </td>  
+      `;
+      listProduct.appendChild(row)
+}
