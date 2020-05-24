d6fcec6c1f5d9b600b50279789b9bb6deb8e8d23
Isam David <isamdavid17@gmail.com>
2020-05-24 15:23:15 -0500

    Se agrego el evento para vaciar el carrito

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -21,42 +21,35 @@ function hideShowCar() {
 const car = document.querySelector('.car__nav')
 const products = document.querySelector('.main')
 const listProduct = document.querySelector('#car__list tbody')
-let cant = 1;
+const emptyCar = document.querySelector('.vaciar-carrito')
 let listIds = []
-// let globalProduct;
 
-
-// console.log(car)
-// console.log(products)
-
-//listeners
 chargeEventListeners()
 
 function chargeEventListeners(){
   //Dispara cuando presionamos "Añadir"
   products.addEventListener('click',buyProducts);
+  //Borramos los productos desde el boton de la X
   listProduct.addEventListener('click',deleteProduct);
+  //Vaciamos todo el carrito 
+  emptyCar.addEventListener('click',emptyCarEvent);
 }
 
 
 //funciones
 
-//funci´n que añade producto al carrito 
+//función que añade producto al carrito 
 function buyProducts(e) {
   e.preventDefault();
   //Delegation para agregar al carrito
   if (e.target.classList.contains('card__button')){
     const product = e.target.parentElement.parentElement;
-    // console.log(product);
-    // debugger
     //Enviamos el curso seleccionado para tomar sus datos 
   readProduct(product)
   }
-  
 }
 
 //Esta función lee el producto y guarda sus datos
-
 function readProduct(product){
   const dataProduct = {
     img: product.querySelector('img').src,
@@ -64,7 +57,6 @@ function readProduct(product){
     price: product.querySelector('.price').textContent,
     id: product.querySelector('input').getAttribute('id')
   }
-
   insertCar(dataProduct);
   // console.log(product.querySelector('input').getAttribute('id'))
 }
@@ -91,24 +83,21 @@ function insertCar(product){
         <a href="#" class="delete-product" id="${product.id}">X </a> 
       </td>  
         `;
-        // console.log(row);
         listProduct.appendChild(row)
         console.log(listIds);        
     }
 }
 
-
 //Buscar si existe el elemento en el carrito
 function findSelectedAdd(product){
   let verify = 0;
   listIds.forEach(element => {
-    console.log(`Lista de Encontrados: ${element}`);
     if(product.id == element) return verify = 1;
   });
   return verify;
 }
 
-//elimina el curso del carrito en el dom 
+//elimina el producto del carrito en el dom, solo elimina 1 producto a la vez.
 function deleteProduct(e){
   e.preventDefault();
   
@@ -121,58 +110,22 @@ function deleteProduct(e){
     let idCurrent = e.target.getAttribute('id');
     removeListIds(idCurrent);
     e.target.parentElement.parentElement.remove();
-    //Si la cantidad es igual a 1 entonces hay que eliminar el producto de la lista
   }
   
 }
 function removeListIds(id){
+  //Si la cantidad es igual a 1 entonces hay que eliminar el producto de la lista
   if(listIds.length == 1){
     listIds.pop();
   }else{
     const arrayAux = listIds.filter(ids=>ids!=id);
     listIds = arrayAux;
-    console.log(listIds);
-    // for(let i=0; i< listIds.length; ++i){
-    //   console.log(`Lista de eliminados: ${listIds[i]}`);
-    //   if (id == listIds[i]){
-    //     aux=listIds[listIds.length-1]
-    //     listIds[listIds.length - 1]= listIds[i];
-    //     listIds[aux];
-    //     listIds.pop();
-    //     // listIds.remove(listIds[i]);
-    //     return;
-    //   }
-    // }
   }
 }
-
-
-  // console.log('Eliminado');
-  // let cant
-  // let idx
-  
-  // console.log(globalProduct);
-  
-  // idx = document.getElementById(globalProduct.id)
-  // cant = parseInt(document.getElementById(globalProduct.id).innerText) 
-  // if(cant==1){
-    // const prueba= e.target.classList.contains('id')
-    // console.log(prueba)
-    
-    
-    // const rowCar=e.target.parentElement.parentElement
-    // console.log(rowCar.querySelector('td').getAttribute('id'))
-    // console.log(rowCar.querySelector('td'))
-    // console.log(rowCar);
-    
-    
-    // console.log(rowCar.querySelector('td'))
-    // console.log(cant)
-    // console.log(rowCar.querySelector('td #id').innerText);
-    
-  //   if(e.target.classList.contains('delete-product')){
-  //     // 
-  //     // 
-  //   // } 
-  // } 
-
+//Función para vaciar todo el carrito
+function emptyCarEvent(e){
+  while (listProduct.firstChild){
+    listProduct.removeChild(listProduct.firstChild)
+  }
+  listIds = [];
+}
