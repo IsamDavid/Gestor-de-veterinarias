ce73b58b1047386a682d89426d6b7835c8759727
Isam David <isamdavid17@gmail.com>
2020-05-27 23:24:35 -0500

    Se arreglo el desplazamiento del menú y su tamaño

--- src/js/menuDesplegable.js
+++ src/js/menuDesplegable.js
@@ -4,16 +4,20 @@
 const nav = document.querySelector('.header__nav')
 const burgerButton = document.querySelector('.header__burger')
 const burgerText = document.querySelector('.header__burger--text')
-// console.log(burgerText)
 
 burgerButton.addEventListener('click', hideShow)
-// burgerText = addEventListener('click',hideShow)
 
 function hideShow() {
   if (nav.classList.contains('is_activate')) {
     nav.classList.remove('is_activate')
+    setTimeout(() => {
+      nav.classList.remove('hide')
+    }, 500)
   }
   else {
-    nav.classList.add('is_activate')
+    nav.classList.add('hide')
+    setTimeout(()=>{
+      nav.classList.add('is_activate')
+    },100)
   }
 }
